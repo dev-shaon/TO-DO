7e8fbeee2432c167ea2e80d440c48133e9519d2a
Shaon Das <shaondas675@gmail.com>
2025-11-06 22:59:02 +0600

    Add new sccreen

--- lib/Screens/sign_up.dart
+++ lib/Screens/sign_up.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:to_do/Screens/Home_Screen.dart';
-import 'package:to_do/Screens/fristScreen.dart';
 import 'package:to_do/Screens/sign_in.dart';
 
 class SignUp extends StatefulWidget {
