8b331b0cad76a612a2b59e2a68fdd207010d3f15
Shaon Das <shaondas675@gmail.com>
2025-12-01 21:48:25 +0600

    update some code

--- lib/Screens/Profile Screen/Changee_Username.dart
+++ lib/Screens/Profile Screen/Changee_Username.dart
@@ -12,13 +12,11 @@ class _ChangeeUsernameState extends State<ChangeeUsername> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(""),
+        backgroundColor: const Color.fromARGB(255, 9, 56, 210),
+        title: Text("Change Username",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),),
+        centerTitle: true,
       ),
-      body: Column(
-        children: [
-          Center(child: Text("data"),)
-        ],
-      ),
+      body: Column(children: [Center(child: Text("data"))]),
     );
   }
 }
