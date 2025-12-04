590de8920269efe3d824e926386adb4ee4b1853c
Shaon Das <shaondas675@gmail.com>
2025-12-04 20:28:34 +0600

    some change code

--- lib/Screens/Profile Screen/Change_pass.dart
+++ lib/Screens/Profile Screen/Change_pass.dart
@@ -11,10 +11,31 @@ class _ChangePassState extends State<ChangePass> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Center(child: Text("ChangePass"),)
-        ],
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 9, 56, 210),
+        title: Text(
+          "Change Password",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            SizedBox(height: 50,),
+            TextField(
+              decoration: InputDecoration(
+                fillColor: const Color.fromARGB(255, 211, 234, 247),
+                filled: true,
+                hintText: "current passwrd",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20)
+                )
+              ),
+            )
+              ]
+              ),
       ),
     );
   }
